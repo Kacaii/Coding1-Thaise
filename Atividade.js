3c2a61874df400966c0ea7074bd9a30547843e83
THAISERENAUX <thaise.renaux@edu.pe.senac.br>
2024-09-25 10:39:49 -0300

    criei o repositório

---
+++ Atividade.js
@@ -0,0 +1,36 @@
+let nome = 'Thaise'
+let idade = 29
+let cidade = 'Olinda'  
+
+console.log (`Olá, meu nome é ${nome} e tenho ${idade} anos`)
+
+console.log (`${nome} é de ${cidade}`)
+
+let num1 = 1
+let num2 = 2
+
+let soma = num1 + num2
+let subtracao = num1 - num2
+let multiplicacao = num1 * num2
+let divisao = num1 / num2
+
+console.log (soma, subtracao, multiplicacao, divisao);
+
+let base = 10
+let altura = 5
+let area = (base * altura) / 2
+
+console.log ('A área do triângulo é ' + area)
+
+let nota1 = 10
+let nota2 = 8
+let nota3 = 6.5
+
+let media = (nota1 + nota2 + nota3) / 3 
+
+console.log ('A média do aluno é ' + media)
+
+console.log (`A média do aluno é ${media}`)
+
+let preço = 1000
+let percentualDesconto
---
+++ Aula.js
@@ -0,0 +1,15 @@
+
+var prompt = require('prompt-sync')();
+
+let nome = prompt("Qual o seu nome?")
+
+console.log(`Olá, ${nome}! Bem vinda à faculdade Senac`)
+
+let idade = prompt("Qual a sua idade?")
+
+console.log(`Ok, senhora.`)
+
+let ajuda = prompt ('Como posso te ajudar?')
+
+console.log('Você que lute!')
+
